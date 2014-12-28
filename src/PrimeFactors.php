f759dc86d63b27b91461c55510184708e35f8b43
Frank Perez <frank@frankperez.net>
2014-12-28 09:59:32 -0500

    Initial commit.

---
+++ src/PrimeFactors.php
@@ -0,0 +1,24 @@
+<?php
+
+class PrimeFactors
+{
+
+    public function generate($number)
+    {
+        $primes = [];
+
+        for($candidate = 2; $number > 1; $candidate++) {
+            for(; $number % $candidate == 0; $number /= $candidate)
+            {
+                $primes[] = $candidate;
+            }
+        }
+
+        if($number > 1)
+        {
+            $primes[] = $number;
+        }
+
+        return $primes;
+    }
+}
